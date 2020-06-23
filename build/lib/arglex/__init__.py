3ab766489002ad4240b975a894c44a50f64d1e95
Mathias Haugestad <mhaugestad@gmail.com>
2020-06-23 23:34:55 +0100

    packaged

--- build/lib/arglex/__init__.py
+++ build/lib/arglex/__init__.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .assessment import Assessment
-from .authority import Authority
+from assessment.assessment import Assessment
+from authority.authority import Authority
+from causation.causation import Causation
+from conditionals.conditionals import Conditionals
+from contrast.contrast import Contrast
+from difficulty.difficulty import Difficulty
+from doubt.doubt import Doubt
+from emphasis.emphasis import Emphasis
+from generalization.generalization import Generalization
+from inconsistency.inconsistency import Inconsistency
+from intensifiers.intensifiers import Intensifiers
+from inyourshoes.inyourshoes import Inyourshoes
+from necessity.necessity import Necessity
+from possibility.possibility import Possibility
+from priority.priority import Priority
+from rhetoricalquestion.rhetoricalquestion import RhetoricalQuestion
+from spoken.spoken import Spoken
+from structure.structure import Structure
+from wants.wants import Wants
+from wordclasses.wordclasses import Wordclasses
