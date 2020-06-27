699cf3a38dd7cfca36cd23f1dae26564490a8c0d
Mathias Haugestad <mhaugestad@gmail.com>
2020-06-27 13:54:51 +0100

    update setup

--- arglex/__init__.py
+++ arglex/__init__.py
@@ -2,4 +2,5 @@
 # -*- coding: utf-8 -*-
 
 from .spacy_arglex import arglex
-from .spacy_arglex import load_testfile
+from .spacy_arglex import load_test_patterns
+from .spacy_arglex import set_token_extensions
